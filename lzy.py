774cbb43633d5e765e4bcf12e61c4775675bff4b
李子扬 <513523798@qq.com>
2019-10-08 20:43:16 +0800

    first commit

---
+++ lzy.py
@@ -0,0 +1,9 @@
+def cut_sentences(sentence):  
+      puns = frozenset(u'.')   
+       tmp = []    
+       for ch in sentence:       
+            tmp.append(ch)        
+            if puns.__contains__(ch):         
+                   yield ''.join(tmp)          
+                   tmp = []   
+         yield ''.join(tmp)
